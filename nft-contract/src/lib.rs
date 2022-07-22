af97ea292098638fe8e5ebd47ca37dddafe58685
AllSkillsca <109824969+AllSkillsca@users.noreply.github.com>
2022-07-22 16:08:05 -0400

    Add files via upload

---
+++ nft-contract/src/lib.rs
@@ -0,0 +1,157 @@
+use std::collections::HashMap;
+use std::cmp::min;
+
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::collections::{LazyOption, LookupMap, UnorderedMap, UnorderedSet};
+use near_sdk::json_types::{Base64VecU8, U128, U64};
+use near_sdk::serde::{Deserialize, Serialize};
+use near_sdk::{
+    env, near_bindgen, AccountId, Balance, CryptoHash, PanicOnDefault, Promise, PromiseOrValue,
+};
+
+use crate::internal::*;
+pub use crate::metadata::*;
+pub use crate::mint::*;
+pub use crate::nft_core::*;
+pub use crate::approval::*;
+pub use crate::royalty::*;
+pub use crate::events::*;
+
+mod internal;
+mod approval; 
+mod enumeration; 
+mod metadata; 
+mod mint; 
+mod nft_core; 
+mod royalty; 
+mod events;
+
+/// This spec can be treated like a version of the standard.
+pub const NFT_METADATA_SPEC: &str = "1.0.0";
+/// This is the name of the NFT standard we're using
+pub const NFT_STANDARD_NAME: &str = "nep171";
+
+pub type TokenType = String;
+pub type TypeSupplyCaps = HashMap<TokenType, U64>;
+
+#[near_bindgen]
+#[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
+pub struct Contract {
+    //contract owner
+    pub owner_id: AccountId,
+
+    //keeps track of all the token IDs for a given account
+    pub tokens_per_owner: LookupMap<AccountId, UnorderedSet<TokenId>>,
+
+    //keeps track of the token struct for a given token ID
+    pub tokens_by_id: LookupMap<TokenId, Token>,
+
+    //keeps track of the token metadata for a given token ID
+    pub token_metadata_by_id: UnorderedMap<TokenId, TokenMetadata>,
+
+    //keeps track of the metadata for the contract
+    pub metadata: LazyOption<NFTContractMetadata>,
+
+    //Custom
+    pub supply_cap_by_type: TypeSupplyCaps,
+    pub tokens_per_type: LookupMap<TokenType, UnorderedSet<TokenId>>,
+    pub token_types_locked: UnorderedSet<TokenType>,
+
+}
+
+/// Helper structure for keys of the persistent collections.
+#[derive(BorshSerialize)]
+pub enum StorageKey {
+    TokensPerOwner,
+    TokenPerOwnerInner { account_id_hash: CryptoHash },
+    TokensById,
+    TokenMetadataById,
+    NFTContractMetadata,
+    TokensPerType,
+    TokensPerTypeInner { token_type_hash: CryptoHash },
+    TokenTypesLocked,
+}
+
+#[near_bindgen]
+impl Contract {
+    pub(crate) fn assert_owner(&self) {
+        assert_eq!(&env::predecessor_account_id(), &self.owner_id, "Owner's method")
+    }
+    /*
+        initialization function (can only be called once).
+        this initializes the contract with default metadata so the
+        user doesn't have to manually type metadata.
+    */
+    #[init]
+    pub fn new_default_meta(owner_id: AccountId) -> Self {
+        //calls the other function "new: with some default metadata and the owner_id passed in 
+        Self::new(
+            owner_id,
+            NFTContractMetadata {
+                spec: "nft-1.0.0".to_string(),
+                name: "AllSkills Alpha NFTs".to_string(),
+                symbol: "AllSkills".to_string(),
+                icon: None,
+                base_uri: None,
+                reference: None,
+                reference_hash: None,
+            },
+            HashMap::new(),
+            Some(true)
+        )
+    }
+
+    /*
+        initialization function (can only be called once).
+        this initializes the contract with metadata that was passed in and
+        the owner_id. 
+    */
+    #[init]
+    pub fn new(owner_id: AccountId, metadata: NFTContractMetadata, supply_cap_by_type:TypeSupplyCaps, unlocked:Option<bool>) -> Self {
+        //create a variable of type Self with all the fields initialized. 
+        let this = Self {
+            //Storage keys are simply the prefixes used for the collections. This helps avoid data collision
+            tokens_per_owner: LookupMap::new(StorageKey::TokensPerOwner.try_to_vec().unwrap()),
+            tokens_by_id: LookupMap::new(StorageKey::TokensById.try_to_vec().unwrap()),
+            token_metadata_by_id: UnorderedMap::new(
+                StorageKey::TokenMetadataById.try_to_vec().unwrap(),
+            ),
+            //set the owner_id field equal to the passed in owner_id. 
+            owner_id,
+            metadata: LazyOption::new(
+                StorageKey::NFTContractMetadata.try_to_vec().unwrap(),
+                Some(&metadata),
+            ),
+            supply_cap_by_type,
+            tokens_per_type: LookupMap::new(StorageKey::TokensPerType.try_to_vec().unwrap()),
+            token_types_locked: UnorderedSet::new(StorageKey::TokenTypesLocked.try_to_vec().unwrap()),
+        };
+
+        //return the Contract object
+        this
+    }
+    pub fn add_token_types(&mut self, supply_cap_by_type: TypeSupplyCaps, unlocked: Option<bool>) {
+        self.assert_owner();
+        for (token_type, hard_cap) in &supply_cap_by_type {
+            if unlocked.is_none() {
+                assert!(self.token_types_locked.insert(&token_type), "Token type should not be locked");
+            }
+            assert!(self.supply_cap_by_type.insert(token_type.to_string(), *hard_cap).is_none(), "Token type exists");
+        }
+    }
+    pub fn unlock_token_types(&mut self, token_types: Vec<String>) {
+		self.assert_owner();
+        for token_type in &token_types {
+            self.token_types_locked.remove(&token_type);
+        }
+    }
+    pub fn is_token_locked(&self, token_id: TokenId) -> bool {
+        let token = self.tokens_by_id.get(&token_id).expect("No token");
+        assert_eq!(token.token_type.is_some(), true, "Token must have type");
+        let token_type = token.token_type.unwrap();
+        self.token_types_locked.contains(&token_type)
+    }
+    pub fn get_supply_caps(&self) -> TypeSupplyCaps {
+        self.supply_cap_by_type.clone()
+    }
+}
